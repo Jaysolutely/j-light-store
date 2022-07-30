71e07d84dcb9b9c0ddfde6cfc29333ac7f434fc9
jay <julius.rahaus@qciety.org>
2022-07-30 08:46:27 +0200

    add DefaultAction

--- src/types.ts
+++ src/types.ts
@@ -8,13 +8,17 @@ export type StoreState = Record<string, state>;
 export interface Action {
   type: string;
 }
+interface ActionDefault extends Action {
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  [key: string]: any
+}
 export type render = (store: Store) => void;
 export type dispatchCallback<CS = state> = (state: CS) => void;
-export type reducer<CA extends Action = Action, CS = state> = (
+export type reducer<CA extends Action = ActionDefault, CS = state> = (
   action: CA,
   state: CS
 ) => CS;
-export type extendedDispatch<CA extends Action = Action, CS = state> = (
+export type extendedDispatch<CA extends Action = ActionDefault, CS = state> = (
   action: CA,
   name?: string,
   callback?: dispatchCallback<CS>
